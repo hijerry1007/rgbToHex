5c0ce182ea9c3c0eeba3dfd76679679e3f1e4261
Jerry Lai <jerrydavid123@hotmail.com>
2020-01-06 19:05:13 +0800

    createRGBtoHex

---
+++ rgbtohex.markdown
@@ -0,0 +1,7 @@
+rgbtohex
+--------
+
+
+A [Pen](https://codepen.io/JerryLAI/pen/MWYrEoK) by [賴志承](https://codepen.io/JerryLAI) on [CodePen](https://codepen.io).
+
+[License](https://codepen.io/JerryLAI/pen/MWYrEoK/license).
---
+++ script.js
@@ -0,0 +1,63 @@
+const Red = document.getElementById('r')
+const Green = document.getElementById('g')
+const Blue = document.getElementById('b')
+const input = document.querySelectorAll('input')
+
+input.forEach(input=>{input.addEventListener('change', function change(){
+  let red = `${Red.value}`
+  let green = `${Green.value}`
+  let blue = `${Blue.value}`
+  let hex = fullColorHex(red,green,blue)
+  document.getElementById('hexPanel').innerHTML = hex 
+  document.documentElement.style.setProperty(`--base`, hex)
+  
+})})
+
+input.forEach(input=>{input.addEventListener('mousemove', function change(){
+  let red = `${Red.value}`
+  let green = `${Green.value}`
+  let blue = `${Blue.value}`
+  let hex = fullColorHex(red,green,blue)
+  document.getElementById('hexPanel').innerHTML = hex 
+  document.documentElement.style.setProperty(`--base`, hex)
+  
+})})
+
+function inputValueR(x){
+  let valueR = document.getElementById(x).value
+  numberR = valueR
+　document.getElementById('valueR').innerHTML = valueR 
+  return valueR  
+}
+
+
+function inputValueG(x){
+　let valueG = document.getElementById(x).value
+  numberG = valueG
+　document.getElementById('valueG').innerHTML = valueG
+  return valueG
+}
+
+function inputValueB(x){
+　let valueB = document.getElementById(x).value;
+  numberB = valueB
+　document.getElementById('valueB').innerHTML = valueB
+  return valueB
+}
+
+function rgbToHex (rgb) { 
+  let hex = Number(rgb).toString(16);
+  if (hex.length < 2) {
+       hex = "0" + hex;
+  }
+  return hex;
+}
+
+function fullColorHex(r,g,b) {   
+  let red = rgbToHex(r);
+  let green = rgbToHex(g);
+  let blue = rgbToHex(b);
+  return "#" + red+green+blue;
+}
+
+
